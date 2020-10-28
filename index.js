9fd0acb5441a7afe44658da70e2042fb7ed80650
Yousif Faraj <yfaraj96@gmail.com>
2020-10-28 16:01:01 -0400

    updated proxy for deployment

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
+const axios = require('axios');
 const app = express();
 
 const PORT = 3000;
-const HOST = 'localhost';
 
+app.use(express.json())
 app.use(express.static(__dirname));
 
 app.get('/info', (req, res, next) => {
@@ -15,6 +15,18 @@ app.get('/*', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+app.post('/api/products', (req, res) => {
+  var info = req.body
+  axios.post('http://3.139.9.29:3003/api/products', info).catch(err => console.error(err)).then(() => res.send('Done'));
+})
+
+app.listen(PORT, () => {
+  console.log(`Starting Proxy at ${PORT}`);
 });
+
+
+
+
+
+
+
